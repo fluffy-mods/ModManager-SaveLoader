3d61bd0d5e38019533cc3eb48112690ba538704a
Fluffy <fluffy.l2032@gmail.com>
2019-10-14 22:29:32 +0200

    initial commit

---
+++ Source/SaveLoader.cs
@@ -0,0 +1,50 @@
+﻿// SaveLoader.cs
+// Copyright Karel Kroeze, 2019-2019
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace SaveLoader
+{
+    public class SaveLoader : Verse.Mod
+    {
+        public SaveLoader( ModContentPack mod ) : base( mod )
+        {
+            if ( GenCommandLine.TryGetCommandLineArg( "save", out string saveName ) )
+                InitiateSaveLoading( saveName );
+        }
+
+        // lifted wholesale from HugsLib, with minor alterations
+        public static void InitiateSaveLoading( string saveName )
+        {
+            if ( saveName == null )
+            {
+                throw new WarningException( "save filename not set" );
+            }
+
+            var filePath = GenFilePaths.FilePathForSavedGame( saveName );
+            if ( !File.Exists( filePath ) )
+            {
+                throw new WarningException( "save file not found: " + filePath );
+            }
+
+            Log.Message( "Starting saved game: " + saveName );
+            Action loadAction = () =>
+            {
+                LongEventHandler.QueueLongEvent(
+                    delegate { Current.Game = new Game {InitData = new GameInitData {gameToLoad = saveName}}; },
+                    "Play", "LoadingLongEvent", true, null );
+            };
+
+            PreLoadUtility.CheckVersionAndLoad( filePath, ScribeMetaHeaderUtility.ScribeHeaderMode.Map, loadAction );
+        }
+
+    }
+}
